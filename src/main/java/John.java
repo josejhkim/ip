dbb3b15491cac9c978755b7081a633b9bd415aa2
josejhkim <barsamic03@gmail.com>
2025-01-22 12:57:00 +0800

    change greeting message with new chatbot name

--- src/main/java/John.java
+++ src/main/java/John.java
@@ -1,10 +1,9 @@
 public class John {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm John, your personalized assistant chatbot");
+        System.out.println("How can I help you today?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Goodbye and have a nice day!");
     }
 }
