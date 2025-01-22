26ce4ec01f1c18c1ed8b4e9501251f5bca9374c8
josejhkim <barsamic03@gmail.com>
2025-01-22 18:50:39 +0800

    import scanner for user input

--- src/main/java/John.java
+++ src/main/java/John.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
+
 public class John {
     public static void main(String[] args) {
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm John, your personalized assistant chatbot");
         System.out.println("How can I help you today?");
+
+        Scanner myObj = new Scanner(System.in);
+        String userInput = myObj.nextLine();
+
         System.out.println("____________________________________________________________");
         System.out.println("Goodbye and have a nice day!");
     }
