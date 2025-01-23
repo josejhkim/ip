b90f4bfc82e2a4d7af8345acee4b768ef7d74bc3
josejhkim <barsamic03@gmail.com>
2025-01-23 09:36:23 +0800

    add functionality for marking/unmarking tasks

--- src/main/java/John.java
+++ src/main/java/John.java
@@ -12,10 +12,26 @@ public class John {
         Scanner myObj = new Scanner(System.in);
         String userInput = myObj.nextLine();
         List<Task> taskList = new ArrayList<>();
-        
+
         while (!userInput.equals("bye")) {
             System.out.println("____________________________________________________________");
-            if (userInput.equals("list")) {
+            if (userInput.startsWith("mark ")) {
+                int index = Integer.parseInt(userInput.substring(5)) - 1;
+                if (index >= taskList.size()) {
+                    System.out.println("index " + (index + 1) + " is out of bounds for your list of size " + taskList.size());
+                } else {
+                    System.out.println("marking \"" + taskList.get(index).getDescription() + "\" as done!");
+                    taskList.get(index).markAsDone();
+                }
+            } else if (userInput.startsWith("unmark ")){
+                int index = Integer.parseInt(userInput.substring(7)) - 1;
+                if (index >= taskList.size()) {
+                    System.out.println("index " + (index + 1) + " is out of bounds for your list of size " + taskList.size());
+                } else {
+                    System.out.println("marking \"" + taskList.get(index).getDescription() + "\" as not done!");
+                    taskList.get(index).unmarkAsDone();
+                }
+            } else if (userInput.equals("list")) {
                 if (taskList.isEmpty()) {
                     System.out.println("your list is currently empty!");
                     System.out.println("type in any item to add it to your list!");
