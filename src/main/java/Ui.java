f6f68bddc3adcfad150c321c2fa15ae1050874f6
josejhkim <barsamic03@gmail.com>
2025-01-30 12:19:42 +0800

    Create class for user interaction messages

---
+++ src/main/java/Ui.java
@@ -0,0 +1,16 @@
+public class Ui {
+    public void printHello() {
+        linebreak();
+        System.out.println("Hello! I'm John, your personalized assistant chatbot");
+        System.out.println("How can I help you today?");
+    }
+
+    public void printGoodbye() {
+        System.out.println("Goodbye and have a nice day!");
+        linebreak();
+    }
+
+    public void linebreak() {
+        System.out.println("------------------------------------------------------------");
+    }
+}
